0b331670af6d60b22638c1ec6db4e745837f8225
Giovani SFPV <giovanisfpv.com>
2026-05-19 10:18:36 -0300

    changed monitors config

--- monitors.lua
+++ monitors.lua
@@ -1,9 +1,9 @@
 local username = os.getenv("USER") or os.getenv("USERNAME")
 
 if username == "giovani" then
-	hl.notification.create({ text = "configuring giovani monitors", duration = 2000 })
+	hl.monitor({ output = "eDP-1", mode = "1920x1080@60", position = "2560x0", scale = 1 })
+	hl.monitor({ output = "HDMI-A-1", mode = "2560x1080@60", position = "0x0", scale = 1 })
 elseif username == "clinica" then
-	hl.notification.create({ text = "configuring clinica monitors", duration = 2000 })
+	hl.monitor({ output = "VGA-1", mode = "1360x768@60", position = "0x0", scale = 1 })
+	hl.monitor({ output = "HDMI-A-1", mode = "2560x1080@60", position = "1360x0", scale = 1 })
 end
-hl.monitor({ output = "eDP-1", mode = "1920x1080@60", position = "2560x0", scale = 1 })
-hl.monitor({ output = "HDMI-A-1", mode = "2560x1080@60", position = "0x0", scale = 1 })
