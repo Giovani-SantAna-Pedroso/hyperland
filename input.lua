e6200066aedcdd905a4826c1b9e470e06354d6e8
Giovani Sant'Ana Pedroso <giovanisfpv@gmail.com>
2026-09-03 22:19:27 -0300

    touchpad sensitivity changed

--- input.lua
+++ input.lua
@@ -8,10 +8,10 @@ hl.config({
 
 		follow_mouse = 1,
 
-		sensitivity = 0.6, -- -1.0 - 1.0, 0 means no modification.
+		sensitivity = 0.4, -- -1.0 - 1.0, 0 means no modification.
 
 		touchpad = {
-			natural_scroll = false,
+			natural_scroll = true,
 		},
 	},
 })
